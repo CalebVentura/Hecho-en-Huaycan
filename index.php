af411b1aa799dd85e952bc612f1ef755e537502b
Caleb Ventura Vasquez <caleb.ventura.v@uni.pe>
2018-08-03 02:43:37 -0500

    Primer commit HeH
    Este es el primer commit que se hace despues de haberlo escrito còdigo para la barra de navegacion

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+        <div class="barra">
+            <ul>
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">NOTICIAS</a></li>
+                <li><a href="#">MARCAS</a></li>
+                <li><a href="#">REPORTAJES</a></li>
+                <li><a href="#">HISTORIA</a></li>
+                <li><a href="#">MENSAJES</a></li>
+            </ul>
+        </div>
+
+<?php include_once 'includes/templates/footer.php'; ?>
+
+
